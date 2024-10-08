2827b16e7ba3f4744ddb7067656835609b449968
derek <derek0717@gmail.com>
2024-10-08 15:40:12 +0800

    update fs methods for node 18

--- line2wtsapp.js
+++ line2wtsapp.js
@@ -78,16 +78,16 @@ https.get(linePackRemotePath, function(response) {
                             if (!file.match(/^[0-9]*@2x\.png$/)) {
                                 return;
                             }
-                            fs.copyFileSync(path.join(staticInputPath, file), path.join(packOutputPath, file), (err) => {
-                                if (err) {
-                                    console.error('failed to copy '+file, err);
-                                    throw err;
-                                }
-                            });
+                            try {
+                                fs.copyFileSync(path.join(staticInputPath, file), path.join(packOutputPath, file));
+                            } catch (e){
+                                console.error('failed to copy ' + file, err);
+                                throw err;
+                            }
                             console.log('copied '+file);
                         });
                         console.log('static all done.');
-                        fs.rmdir(unzippedPackPath, { recursive: true },function(){
+                        fs.rm(unzippedPackPath, { recursive: true },function(){
                             return;
                         })
                     });
